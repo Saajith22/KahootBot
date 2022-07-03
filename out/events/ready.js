1e3c3c7090c2eb25329a20256e7f1939e41d5faa
Saajith22 <mohammedsaajith93@gmail.com>
2022-07-03 11:56:33 +0400

    remove out

--- out/events/ready.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("../index"));
-index_1.default.on("ready", () => {
-    console.log(`${index_1.default.user.username} is now ready to play!!`.magenta);
-});
--- out/utils/Command.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Command {
-    constructor(params) {
-        return params;
-    }
-}
-exports.default = Command;
